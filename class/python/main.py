2ab8d7663d486fd3adc39c0d8f36a328b95c8435
Pradeep Chaudhary <cgpradeepop@gmail.com>
2026-08-12 13:17:47 +0545

    Add class hierarchy with data encapsulation and display method in main.py

--- class/python/main.py
+++ class/python/main.py
@@ -1,1 +1,23 @@
 print("Hello, Pradeep")
+
+class A:
+    def __init__(self, name: str, home: str) -> str:
+        self.name = name
+        self.home = home
+
+class B(A):
+    def __init__(self, name: str, home: str, newhome: str):
+        super().__init__(name, home)
+        self.newhome = newhome
+
+class C(B):
+    def __init__(self, name: str, home: str, newhome: str, car: str):
+        super().__init__(name, home, newhome)
+        self.car = car
+
+    def all_data(self):
+        return f"\nname: {self.name}\nhome: {self.home}\nnewhome: {self.newhome}\ncar: {self.car}"
+
+data = C("pradeep", "mnr", "ktm", "bmw")
+
+print(data.all_data())
